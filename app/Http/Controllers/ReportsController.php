9e1e4fc470fc6b2b1460db3821aadcdcf2c49dd8
unknown <drana@contriverz.com>
2019-01-08 17:08:11 +0530

    working on report record function'(8 dec 2018) at 5 pm

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Report;
 use App\Project;
+use App\Action;
 use App\Group;
 use App\User;
+use App\Question;
+use App\QuesReply;
 use Illuminate\Support\Facades\Auth;
 use App\Collaboration;
 use App\Group_Member;
@@ -108,4 +111,175 @@ class ReportsController extends Controller
 
     }
 
+    public function getAction(Request $request){
+
+      $project_id = $request->project_id;
+      $project_name = $request->project_name;
+      $data_value = $request->data_value;
+      $project_path = $request->project_path;
+
+      $All_actions = [];
+
+            if($data_value == '0')
+            {
+               
+              $actions = Report::where('document_path','LIKE',"%{$project_name}%")->get()->toArray();
+
+                     foreach ($actions as $actions) {
+
+                        $actionTypeId = $actions['action'];
+
+                        $getAction = Action::where('id',$actionTypeId)->first();          
+                        //first
+                        $Action = $getAction['action'];
+
+                        $document = $actions['document_path'];
+
+                        $doucment_regards = explode('/',$document);
+                          
+                        // second
+                        $document_name = end($doucment_regards);
+                       
+                        //third
+                        $Auth = $actions['Auth'];
+
+                        $getAuther = User::where('id',$Auth)->get()->toArray();
+
+                        $auther = $getAuther['0']['name'];
+
+                        //forth
+                        
+                        $time = $actions['created_at'];
+
+                        $action = ['action'=>$Action, 'document'=>$document_name ,'auther'=>$auther, 'time'=> $time];
+
+                        array_push($All_actions, $action);
+
+                     }
+
+            }else{
+
+              foreach ($data_value as $data_value) {
+                   
+                  $actions = Report::where('action',$data_value)->where('document_path','LIKE',"%{$project_path}%")->get()->toArray();
+
+                          foreach ($actions as $actions) {
+
+                                $actionTypeId = $actions['action'];
+
+                                $getAction = Action::where('id',$actionTypeId)->first();          
+                                //first
+                                $Action = $getAction['action'];
+
+                                $document = $actions['document_path'];
+
+                                $doucment_regards = explode('/',$document);
+                                  
+                                // second
+                                $document_name = end($doucment_regards);
+                               
+                                //third
+                                $Auth = $actions['Auth'];
+
+                                $getAuther = User::where('id',$Auth)->get()->toArray();
+
+                                $auther = $getAuther['0']['name'];
+
+                                //forth
+                                
+                                $time = $actions['created_at'];
+
+                                $action = ['action'=>$Action, 'document'=>$document_name ,'auther'=>$auther, 'time'=> $time];
+
+                                array_push($All_actions, $action);
+
+                      }
+
+                 }
+
+            }
+
+        return $All_actions;
+
+    }
+
+    public function getGroupsReports(Request $request){
+
+        $project_id = $request->project_id;
+        $project_name = $request->project_name;
+        $data_value = $request->data_value;
+
+
+        $returnArray1 = [];
+
+        $group_details = [];
+
+        $countUserIsMember = [];
+        
+        foreach ($data_value as $data_value) {
+
+          $totalQuestion = '0';
+            
+          $invitedUser = Group_Member::where('group_id',$data_value)->get()->toArray();
+
+          $getGroup_name = Group::where('id',$data_value)->first()->toArray();
+
+          $group_name = $getGroup_name['group_name'];
+
+          $invitedUserCount = count($invitedUser);
+
+          foreach ($invitedUser as $invitedUser) {
+
+             $UserEmail = $invitedUser['member_email'];
+            
+             $checkUserIsExits = User::Where('email',$UserEmail)->get()->toArray();
+
+             if(!empty($checkUserIsExits))
+               { 
+                 
+                     $userId = $checkUserIsExits['0']['id'];
+
+                     $getPostedQuestionCount = Question::where('send_by',$userId)->get();
+
+                     $countPost = count($getPostedQuestionCount);
+
+                     $totalQuestion = $totalQuestion+$countPost;
+
+                     $get = '1';
+
+                     array_push($countUserIsMember,$get);
+
+               }else{
+
+
+               }
+
+             }
+
+
+          $UserIsMemberOfProject = count($countUserIsMember);
+
+
+          $getPermittedDocument = Permission::where('group_id',$data_value)->get()->toArray();
+          
+          $permittedDocument = count($getPermittedDocument);
+
+          
+          $returnArray = ['group_name'=>$group_name,'invitedUser'=>$invitedUserCount, 'permittedDoc'=>$permittedDocument,'GroupPosted'=>$totalQuestion,'loginUser'=>$UserIsMemberOfProject ];
+
+          array_push($returnArray1,$returnArray);
+
+        }
+
+        return $returnArray1;
+
+      }
+
+
+
+
+
+
+
 }
+
