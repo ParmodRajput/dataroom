d66a44efcaa0d56ded54ebfe165730245ebcd5ee
contriverz <dinesh@contriverz.com>
2019-04-26 17:39:28 +0530

    changres server newhjhfdjfxxxcccccccccccccccccccccccccccxxxxxxcc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2722,9 +2722,7 @@ $(document).on('click','.note1_doc_delete', function(){
     
                            $.each(response.share_view, function(key, value) {
 
-                           html2+='<div class="readall read-more'+key+'" style= display:none><div><b>Date & Time</b>:'+value.time+'</div>'+'<div><b>IP Address</b>:'+value.ip_address+'</div>'+'<div><b>Browser Details</b>:'+value.browser+'</div>'+'<div><b>device</b>:'+value.device+ '</div>'+'</div>';
-
-                            //console.log(value);
+                                //console.log(value);
                             var datetime= value.time;
                             var datetime = datetime.split(" ");
                             console.log(datetime[0],datetime[1]);
@@ -2735,7 +2733,7 @@ $(document).on('click','.note1_doc_delete', function(){
                           html2+='<button onclick="myFunction()" id="myBtn" class="btn  document-btn">Read more</button>';
 
                            var html = "<textarea class='notes_aside_text1' data-value='0' placeholder='Enter text here...' rows='6' cols='28'></textarea><button class='submit_note1_doc'>Add</button><button value='"+documentPath+"' data-value='"+timestamp+"' class='note1_doc_delete hidden'><i class='fa fa-trash-o'></i></button>";
-
+     
                             $('.notes_aside_1').html(html);     
                             $('.notes_aside_text1').val(content);   
                             $('.close_note_aside').removeClass('hidden');
